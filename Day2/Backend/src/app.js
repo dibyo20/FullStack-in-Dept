a9788318d8a6d3ddf35b318b39dc3c3a3fa5212d
dibyo20 <dibyobanerjee005@gmail.com>
2026-03-13 10:47:59 +0530

    Backend Deleted

--- Day2/Backend/src/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const authRouter = require("./routes/auth.routes");
-
-app.use("/api/auth", authRouter);
-
-module.exports = app;
-
--- Day2/Backend/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authRouter = express.Router();
-const authController = require("../controller/auth.controller");
-
-authRouter.post("/signup", authController.signup);
-
-authRouter.post("/signin", authController.login);
-
-module.exports = authRouter;
