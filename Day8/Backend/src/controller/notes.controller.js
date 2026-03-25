90605eb26b62167a6b7522824bcc237c1c76a744
dibyo20 <dibyobanerjee005@gmail.com>
2026-03-25 10:40:57 +0530

    Day8 Created 1 time

---
+++ Day8/Backend/src/controller/notes.controller.js
@@ -0,0 +1,19 @@
+let notes = [];
+
+function createNotes(req, res) {
+    const note = req.body.note;
+    notes.push(note);
+    res.status(201).json({
+        message: "Note Created Successfully",
+        note
+    })
+}
+
+function getNotes(req, res) {
+    res.status(200).json({
+        message: "Notes retrieved successfully",
+        notes
+    })
+}
+
+module.exports = { createNotes, getNotes };
