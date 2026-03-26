594de2e8fffe405c223673a2df6b53c2e185fbf9
dibyo20 <dibyobanerjee005@gmail.com>
2026-03-26 10:03:34 +0530

    Day8 Backend deleted

--- Day8/Backend/server.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const app = require("./src/app.js");
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-    console.log(`Server is listening on: ${PORT}`);
-});
