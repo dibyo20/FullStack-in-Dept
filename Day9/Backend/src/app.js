11770a2f1051e84ef6a9b49d653c7eb3ff43f9a5
dibyo20 <dibyobanerjee005@gmail.com>
2026-03-28 17:59:16 +0530

    Day9 Backend Deleted

--- Day9/Backend/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const notesRouter = require("./routes/notes.route.js");
-
-app.use(express.json());
-
-app.use("/api", notesRouter);
-
-module.exports = app;
--- Day9/Backend/src/models/notes.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const notesSchema = mongoose.Schema({
-    note: String,
-});
-
-const notesModel = mongoose.model("Notes", notesSchema);
-
-module.exports = notesModel;
