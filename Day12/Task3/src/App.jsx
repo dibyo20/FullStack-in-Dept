a2f57daf52febc2580a973524047cf7ec2daa77d
dibyo20 <dibyobanerjee005@gmail.com>
2026-04-05 19:23:00 +0530

    Day12 Task3 Deleted and Created

--- Day12/Task3/src/App.jsx
+++ Day12/Task3/src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [marks, setMarks] = useState([45, 30, 20, 10, 15]);
+  const [marks, setMarks] = useState([45, 30, 20, 15, 10]);
 
-  function graceMarks() {
+  function grace() {
     const newMarks = marks.map(function (elem) {
       if (elem >= 65) {
         return elem;
@@ -17,12 +17,12 @@ const App = () => {
     <div>
       {marks.map(function (elem, idx) {
         return (
-          <h1>
-            Marks of Student {idx + 1} = {elem} ({elem > 30 ? "Pass" : "Fail"})
+          <h1 key={idx}>
+            Marks of Student {idx + 1} = {elem} {elem > 30 ? "Pass" : "Fail"}
           </h1>
         );
       })}
-      <button onClick={graceMarks}>Give Grace</button>
+      <button onClick={grace}>Give Grace</button>
     </div>
   );
 };
