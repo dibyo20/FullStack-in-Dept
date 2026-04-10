eb91107fd419874c3dc90d60f24f53dadc6a1af9
dibyo20 <dibyobanerjee005@gmail.com>
2026-04-10 12:07:04 +0530

    Day13 Backend Created

---
+++ Day13/Backend/src/models/notes.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const notesSchema = new mongoose.Schema({
+    title: {
+        type: String,
+    },
+    description: {
+        type: String,
+    }
+});
+
+
+const notesModel = mongoose.model("Notes", notesSchema);
+
+module.exports = notesModel;
