0ea8a8c7db532bd7df8a90fc763bccf755918e76
dibyo20 <dibyobanerjee005@gmail.com>
2026-05-05 01:50:32 +0530

    Day26 Created "Hashing Implemented"

---
+++ Day26/src/models/auth.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const authSchema = new mongoose.Schema({
+    username: String,
+    email:{
+        type:String,
+        required:true,
+        unique:[true,'Email already exists'],
+    },
+    password:String,
+});
+
+const authModel = mongoose.model('auths',authSchema);
+
+module.exports = authModel;
---
+++ Day26/src/routes/auth.routes.js
@@ -0,0 +1,77 @@
+const authModel = require('../models/auth.model.js');
+const authRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+authRouter.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json({ message: 'Please provide all fields' });
+    }
+
+    const user = await authModel.findOne({ email });
+
+    if (user) {
+        return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const hashedPass = crypto.createHash('md5').update(password).digest('hex');
+
+    const newUser = await authModel.create({ username, email, password: hashedPass });
+    newUser.save();
+
+    res.status(201).json({
+        message: 'User created successfully',
+        user: newUser,
+    });
+});
+
+authRouter.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Please provide all fields' });
+    }
+
+    const user = await authModel.findOne({ email });
+
+    if (!user) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const hashedPass = crypto.createHash('md5').update(password).digest('hex');
+
+    if (user.password !== hashedPass) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET);
+
+    res.cookie('jwt_token', token);
+
+    res.status(200).json({
+        message: 'Login successful',
+        data: {
+            username: user.username,
+            email: user.email,
+        }
+    });
+});
+
+authRouter.post('/protected', (req, res) => {
+    const token = req.cookies.jwt_token;
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    res.status(200).json({ message: 'Protected route accessed' });
+});
+
+authRouter.post('/logout', (req, res) => {
+    res.clearCookie('jwt_token');
+    res.status(200).json({ message: 'Logout successful' });
+});
+
+module.exports = authRouter;
