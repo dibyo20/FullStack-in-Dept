776113ecea1ee530cfc0d98c67c292e7225dad10
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-13 19:22:57 +0530

    feat: implement request validation for user registration and login

--- Day33/src/validators/auth.validator.js
+++ Day33/src/validators/auth.validator.js
@@ -1,1 +1,40 @@
-import { body, param, query, validationResult } from "express-validator"
+const { body, param, query, validationResult } = require("express-validator");
+
+const registerValidator = [
+    body("username").notEmpty().withMessage("Username is required"),
+    body("email").isEmail().withMessage("Invalid email address"),
+    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const error = new Error("Validation failed")
+            error.statusCode = 400
+            error.details = errors.array()
+
+            return next(error)
+        }
+        next();
+    }
+];
+
+const loginValidator = [
+    body("email").isEmail().withMessage("Invalid email address"),
+    body("password").notEmpty().withMessage("Password is required"),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const error = new Error("Validation failed")
+            error.statusCode = 400
+            error.details = errors.array()
+
+            return next(error)
+        }
+        next();
+    }
+];
+
+module.exports = {
+    registerValidator,
+    loginValidator
+}
