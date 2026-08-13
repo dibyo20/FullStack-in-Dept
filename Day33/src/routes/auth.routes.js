01de8bb7a2432bb45a5d938a4c3d90a0a3bac745
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-13 19:25:08 +0530

    feat: add request validation to signup and login routes

--- Day33/src/routes/auth.routes.js
+++ Day33/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 const authRouter = require("express").Router();
 const { register, login, logout, getme } = require("../controllers/auth.controller.js");
+const { registerValidator, loginValidator } = require("../validators/auth.validator.js");
 
 
-authRouter.post("/signup", register);
-authRouter.post("/login", login);
+authRouter.post("/signup", registerValidator, register);
+authRouter.post("/login", loginValidator, login);
 authRouter.post("/logout", logout);
 authRouter.get("/me", getme);
 
