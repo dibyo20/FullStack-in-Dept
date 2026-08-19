112d6686592294ca401d8c48fa0dc4a4a4386728
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-19 18:42:49 +0530

    fix: correct import path for taskSlice in store configuration

--- Mini-Project-1/src/app/store.js
+++ Mini-Project-1/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import taskReducer from "../features/taskSlice.jsx";
+import taskReducer from "../features/taskSlice.js";
 
 export const store = configureStore({
     reducer: {
