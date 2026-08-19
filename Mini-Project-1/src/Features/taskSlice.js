644a66103763119a5c8a192eb361a3271556bdf5
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-19 18:16:54 +0530

    feat: implement task slice with add, delete, and toggle reducers for task management

---
+++ Mini-Project-1/src/Features/taskSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    task =[]
+}
+
+const taskSlice = createSlice({
+    name: "task",
+    initialState,
+    reducers: {
+        addTask: (state, action) => {
+            state.task.push(action.payload);
+        },
+        deleteTask: (state, action) => {
+            state.task = state.task.filter((task) => task.id !== action.payload);
+        },
+        toggleTask: (state, action) => {
+            const task = state.task.find((task) => task.id === action.payload);
+
+            if (task) {
+                task.completed = !task.completed;
+            }
+        }
+    }
+});
+
+export const { addTask, deleteTask, toggleTask } = taskSlice.actions;
+
+export default taskSlice.reducer;
